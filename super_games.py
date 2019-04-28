ed416d7e05b734b60a5e53b2b2a6579c6f0396b2
Soji Omiwade <omiwade@gmail.com>
2019-04-28 14:27:57 -0500

    much better understanding of the bound super method. dont get where you would use the unbound method yet though :(

--- super_games.py
+++ super_games.py
@@ -4,41 +4,42 @@ super(type, obj)
     instancec. so it's an object (of type super). which isn't of type 
     type's imm. parent, but at the same time isn't obj. very interesting
 """
-type(
 class A:
     def __init__(self):
         self.me = 'a'
         print('A object', self, 'created')
     def pooh(self):
         return (self,'A:pooh')
+    @classmethod
+    def glue(cls):
+        return (cls, 'A:glue')
 class B(A):
     def __init__(self):
         self.me = 'b'
-        print('B object', self, 'created')
+        print('object', self, 'initialized in class ', B)
     def pooh(self):
         return str((self,'B:pooh')) + '\n----\n' + str(super().pooh())
     def echo(self):
-        print(super(B, self).pooh()) #this hits A's pooh, not B's!!!
-        
-        #notes
-        print(A.pooh(self) == super(B, self).pooh()) #True!
-        #which means 
-        # print(super(A, self).pooh())
-        
+        print(super(B, self).pooh()) 
+        print(A.pooh(self) == super(B, self).pooh())
+    @classmethod
+    def glue(cls):
+        return (cls, 'B:glue')
+
 class C(B):
     def __init__(self):
-        self.me = 'c'
-        print('C object', self, 'created')
+        self.mee = 'c'
+        print('object', self, 'initialized in class ', C)
     def pooh(self):
         return (self,'C:pooh')
     def echo(self):
-        # print(B.pooh(self))
-        # print(super(C, self).pooh() == B.pooh(self)) 
-        # print(A.pooh(self))
-        # print(super(B, self).pooh() == A.pooh(self)) 
-        print(super().pooh())
-# b = B()
-# b.echo()
-# print()
+        print(B.pooh(self))
+        print(super(C, self).pooh() == B.pooh(self)) 
+        print(A.pooh(self))
+        print(super(B, self).pooh() == A.pooh(self)) 
+        print(super().pooh() == super(C, self).pooh())
+b = B()
+b.echo()
+print()
 c = C()
 c.echo()
