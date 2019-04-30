2b6d4ce9445e323407c5f231bdc8b7b66b3a1007
Soji Omiwade <omiwade@gmail.com>
2019-04-30 16:16:05 -0500

    purpose: showcase how to merge two dictionaries with {} and **

--- dict_merge.py
+++ dict_merge.py
@@ -1,20 +1,25 @@
 import unittest
 
+"""purpose: showcase how to merge two dictionaries with {} and ** """
+
 class DictMergeTestCase(unittest.TestCase):
     def test_1(self):
         x = {'a':1, 'b':2}
         y = {'b':10, 'c': 11}
+        
         xp = x.copy()
         yp = y.copy()
         z = x.update(y)
-
         self.assertDictEqual(y, yp)
         self.assertDictEqual(x, {'a': 1, 'b': 10, 'c': 11})
-        self.assertIsNone(z)
+        self.assertIsNone(z) 
+        #result of above is that x has been overriten, 
+        #and has the expected final result that should've been in z. and z
+        #the variable that should have the final result is None!
         
         z = {**xp, **y}
         self.assertDictEqual(z, x)
-        
+        #voila!
         
 if __name__ == '__main__':
     unittest.main()
