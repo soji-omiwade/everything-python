9ace9cdea33a9ba79779bec52ec7066e29b294c7
Soji Omiwade <omiwade@gmail.com>
2019-04-30 13:15:14 -0500

    purpose: show that the logger module is not reimported to create a new logger. let me be clear: if you reload this module at any time, then you have a new instance, from which yes the logger instance will not be the same as any instance created before the reload. otherwise, all logger instances will be the same

--- shared_variable_test/singleton_get_instance_for_two_clients_test.py
+++ shared_variable_test/singleton_get_instance_for_two_clients_test.py
@@ -2,7 +2,11 @@ import unittest
 import client1
 
 """
-purpose: show that the logger module is not reimported to create a new logger
+purpose: show that the logger module is not reimported to create a new logger.
+let me be clear: if you reload this module at any time, then you have a new 
+instance, from which yes the logger instance will not be the same as any 
+instance created before the reload. otherwise, all logger instances will be the
+same
 """
 class GetInstanceTest(unittest.TestCase):
     def test_(self):
@@ -12,6 +16,11 @@ class GetInstanceTest(unittest.TestCase):
             logger_for_second_client = shoe.Logger.get_instance()
 
             self.assertIs(logger_for_second_client, logger_for_first_client)
-
+            
+            from importlib import reload as rrr
+            rrr(shoe)
+            self.assertIs(logger_for_first_client, logger_for_second_client)
+            self.assertIsNot(logger_for_first_client, shoe.Logger.get_instance())
+            
 if __name__ == '__main__':
     unittest.main()
