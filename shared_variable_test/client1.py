524a14c09ab9b85db1bdf241b39b536f4ffbab83
Soji Omiwade <omiwade@gmail.com>
2019-04-30 17:12:54 -0500

    code clean up

--- shared_variable_test/client1.py
+++ shared_variable_test/client1.py
@@ -1,11 +1,11 @@
 import logger
 import unittest
 
-def c1logger():
+def use_logger():
     return logger.Logger.get_instance()
     
 class create_singleton_logger_instance_test(unittest.TestCase):
     def test(self):
-        self.assertIs(c1logger(), c1logger())
+        self.assertIs(use_logger(), use_logger())
 if __name__ == '__main__':
     unittest.main()
--- shared_variable_test/singleton_get_instance_for_two_clients_test.py
+++ shared_variable_test/singleton_get_instance_for_two_clients_test.py
@@ -2,7 +2,7 @@ import unittest
 import client1
 
 """
-purpose: show that the logger module is not reimported to create a new logger.
+purpose: show that the logger module is not re-imported to create a new logger.
 let me be clear: if you reload this module at any time, then you have a new 
 instance, from which yes the logger instance will not be the same as any 
 instance created before the reload. otherwise, all logger instances will be the
@@ -10,17 +10,18 @@ same
 """
 class GetInstanceTest(unittest.TestCase):
     def test_(self):
-            logger_for_first_client = client1.c1logger()
+            logger_for_first_client = client1.use_logger()
 
-            import logger as shoe
-            logger_for_second_client = shoe.Logger.get_instance()
+            import logger
+            logger_for_second_client = logger.Logger.get_instance()
 
             self.assertIs(logger_for_second_client, logger_for_first_client)
             
             from importlib import reload as rrr
-            rrr(shoe)
+            rrr(logger)
             self.assertIs(logger_for_first_client, logger_for_second_client)
-            self.assertIsNot(logger_for_first_client, shoe.Logger.get_instance())
+            self.assertIsNot(
+            logger_for_first_client, logger.Logger.get_instance())
             
 if __name__ == '__main__':
     unittest.main()
