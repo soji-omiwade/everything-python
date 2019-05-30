741e600ecbe8ee8537c3655aac55c0039f9cd90c
Soji Omiwade <omiwade@gmail.com>
2019-05-30 16:24:16 -0500

    how to use FunctionTestCase. and understanding that behind normal TestCases is creating a new instance of that test case for every test method in the TestCase class!

---
+++ ut/reusing_testcode.py
@@ -0,0 +1,19 @@
+import unittest
+
+def testSomething():
+    l = [2,3,4]
+    assert len(l) is 3
+    
+def setUp():
+    l = [2,3,4]
+
+testcase = unittest.FunctionTestCase(testSomething,)
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(testcase)
+    return suite
+    
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
