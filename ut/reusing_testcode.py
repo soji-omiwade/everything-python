39c73af09d8ab6b951b1d8c7b1e8191a3f95c6bf
Soji Omiwade <omiwade@gmail.com>
2019-05-30 21:13:49 -0500

    more manipulation of old code

--- ut/reusing_testcode.py
+++ ut/reusing_testcode.py
@@ -1,16 +1,26 @@
 import unittest
 
+
 def testSomething():
-    l = [2,3,4]
     assert len(l) is 3
     
-testcase = unittest.FunctionTestCase(testSomething,)
+def foo():
+    global l
+    l = [2,3,4]
+    return l
+    
+testcase = unittest.FunctionTestCase(testSomething,setUp=foo)
 
+class MyNormalTestCase(unittest.TestCase):
+    def test(self):
+        pass
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(testcase)
+    suite.addTest(MyNormalTestCase('test'))
     return suite
     
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
+    runner = unittest.TextTestRunner(verbosity = 2)
     runner.run(suite())
+    # unittest.main()
